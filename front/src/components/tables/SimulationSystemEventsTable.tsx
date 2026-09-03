00009341e6610772786414092a108138ebbe834a
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2026-09-03 16:13:15 -0500

    refactor(frontend): clean up Simulation System Events table.

--- front/src/components/tables/SimulationSystemEventsTable.tsx
+++ front/src/components/tables/SimulationSystemEventsTable.tsx
@@ -12,60 +12,54 @@ type Props = {
   events: SystemEventRecord[];
 };
 
+const systemLabels: Record<string, string> = {
+  "1": "Price Display System",
+  "2": "Corporate Lighting System",
+  "3": "Canopy Lighting System",
+  "4": "Perimeter Lighting System",
+  "5": "Office and General Services System",
+  "6": "Submersible Pump System",
+  "7": "Fuel Dispenser System",
+  "8": "Air Conditioning System - Server Room",
+  "9": "Customer Service Kiosk System - Refrigeration",
+  "10": "Air Conditioning System - Office Area",
+  "11": "Customer Service Kiosk System - Coffee Machine"
+};
+
+const eventTypeLabels: Record<string, string> = {
+  monthly_zero_consumption: "Monthly Zero Consumption"
+};
+
+const formatTimestamp = (
+  timestamp: string
+): string => {
+  const date = new Date(timestamp);
+
+  const formattedDate =
+    date.toLocaleDateString(
+      "en-CA",
+      {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+      }
+    );
+
+  const formattedTime =
+    date.toLocaleTimeString(
+      "en-GB",
+      {
+        hour: "2-digit",
+        minute: "2-digit"
+      }
+    );
+
+  return `${formattedDate} ${formattedTime}`;
+};
+
 export default function SimulationSystemEventsTable({
   events
 }: Props) {
-
-  const systemLabels: Record<string, string> = {
-
-    "1": "Price Display System",
-    "2": "Corporate Lighting System",
-    "3": "Canopy Lighting System",
-    "4": "Perimeter Lighting System",
-    "5": "Office and General Services System",
-    "6": "Submersible Pump System",
-    "7": "Fuel Dispenser System",
-    "8": "Air Conditioning System - Server Room",
-    "9": "Customer Service Kiosk System - Refrigeration",
-    "10": "Air Conditioning System - Office Area",
-    "11": "Customer Service Kiosk System - Coffee Machine"
-
-  };
-
-  const eventTypeLabels: Record<string, string> = {
-
-    monthly_zero_consumption:
-      "Monthly Zero Consumption"
-
-  };
-
-  const formatTimestamp = (
-    timestamp: string
-  ): string => {
-
-    const date = new Date(timestamp);
-
-    const formattedDate =
-      date.toLocaleDateString(
-        "en-CA",
-        {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit"
-        }
-      );
-
-    const formattedTime =
-      date.toLocaleTimeString(
-        "en-GB",
-        {
-          hour: "2-digit",
-          minute: "2-digit"
-        }
-      );
-
-    return `${formattedDate} ${formattedTime}`;
-  };
 
   return (
 
